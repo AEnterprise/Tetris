038d1cda0725cf538a626dae5abe6ab167b51213
AEnterprise <aenterprise2@gmail.com>
2015-05-01 16:23:51 +0200

    so much cleaner now

--- src/tetris/blocks/BigBlock.java
+++ src/tetris/blocks/BigBlock.java
@@ -1,22 +1,27 @@
 package tetris.blocks;
 
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import tetris.screens.ScreenGame;
+import tetris.utils.Location;
 
 /**
  * Created by AEnterprise
  */
 public abstract class BigBlock {
+	protected static Location[][] STATES;
 	public int x, y, rotation;
 	protected Color color;
+	protected List<Location> locations = new ArrayList<>();
 
 	public BigBlock(int x, int y, Color color) {
 		this.x = x;
 		this.y = y;
 		rotation = 0;
 		this.color = color;
-		addBlocks();
 	}
 
 	public void moveDown() {
@@ -40,19 +45,77 @@ public abstract class BigBlock {
 		addBlocks();
 	}
 
-	public abstract void rotate();
+	public void rotate() {
+		removeBlocks();
+		rotateRight();
+		resetList();
+		for (Location location : locations) {
+			if (!ScreenGame.blockManager.isEmpty(x + location.getY(), x + location.getY())) {
+				rotateLeft();
+				resetList();
+				return;
+			}
+		}
+		addBlocks();
+	}
 
-	public abstract void removeBlocks();
+	public void rotateLeft() {
+		if (rotation == 0)
+			rotation = 3;
+		else
+			rotation--;
+	}
 
-	public abstract void addBlocks();
+	public void rotateRight() {
+		if (rotation == 3)
+			rotation = 0;
+		else
+			rotation++;
+	}
 
-	public abstract boolean canMoveDown();
+	protected void resetList() {
+		locations.clear();
+		locations.addAll(Arrays.asList(STATES[rotation]));
+	}
 
-	public abstract boolean canMoveLeft();
+	private void removeBlocks() {
+		for (Location location : locations)
+			ScreenGame.blockManager.removeBlock(x + location.getX(), y + location.getY());
+	}
 
-	public abstract boolean canMoveRight();
+	public void addBlocks() {
+		for (Location location : locations) {
+			ScreenGame.blockManager.setBlock(x + location.getX(), y + location.getY(), new Block(color));
+		}
+	}
 
-	public abstract boolean canRotate();
+	public boolean canMoveDown() {
+		removeBlocks();
+		for (Location location : locations) {
+			if (!isEmpty(x + location.getX(), y + 1 + location.getY()))
+				return false;
+		}
+		return true;
+	}
+
+	public boolean canMoveLeft() {
+		removeBlocks();
+		for (Location location : locations) {
+			if (!isEmpty(x - 1 + location.getX(), y + location.getY()))
+				return false;
+		}
+		return true;
+	}
+
+	public boolean canMoveRight() {
+		removeBlocks();
+		for (Location location : locations) {
+			if (!isEmpty(x + 1 + location.getX(), y + location.getY()))
+				return false;
+		}
+		return true;
+	}
+
 
 	protected boolean isEmpty(int x, int y) {
 		return ScreenGame.blockManager.isEmpty(x, y);
---
+++ src/tetris/utils/Location.java
@@ -0,0 +1,25 @@
+package tetris.utils;
+
+/**
+ * Copyright (c) 2014-2015, AEnterprise
+ * http://buildcraftadditions.wordpress.com/
+ * Buildcraft Additions is distributed under the terms of GNU GPL v3.0
+ * Please check the contents of the license located in
+ * http://buildcraftadditions.wordpress.com/wiki/licensing-stuff/
+ */
+public class Location {
+	private int x, y;
+
+	public Location(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+}
