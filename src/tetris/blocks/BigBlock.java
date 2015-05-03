477f535c2ab16b0ffd42d07ec66a67ec0d5bde43
AEnterprise <aenterprise2@gmail.com>
2015-05-03 13:30:04 +0200

    fix rotation bug found by corjaantje

--- src/tetris/blocks/BigBlock.java
+++ src/tetris/blocks/BigBlock.java
@@ -53,7 +53,7 @@ public class BigBlock {
 		rotateRight();
 		resetList();
 		for (Location location : locations) {
-			if (x + location.getX() < 10 && y + location.getY() < 20 && !ScreenGame.blockManager.isEmpty(x + location.getX(), y + location.getY())) {
+			if (x + location.getX() > 9 || y + location.getY() >19 || !ScreenGame.blockManager.isEmpty(x + location.getX(), y + location.getY())) {
 				rotateLeft();
 				resetList();
 			}
